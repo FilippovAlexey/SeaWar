bbb76e4cba6256e414d1bcb79404d1cdbe953182
Alexey Filippov <lenia335@gmail.com>
2016-08-18 21:36:23 +0300

    rename Messages

---
+++ SeaWarServer/SeaWarServer/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace SeaWarServer
+{
+    public class Messages
+    {
+        public const string WrongRequest = "Wrong request data";
+        public const string UserNotFound = "User not found";
+        public const string Success = "Success";
+        public const string NotFound = "Not Found";
+    }
+}
